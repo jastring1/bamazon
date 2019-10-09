4c27cee441d0f108bc015dbdc37a491720e95649
jastring <tripp.stringfield@gmail.com>
2019-10-09 17:55:18 -0400

    added images and updated readme

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Laptop", "Electronics", 400, 23)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Television", "Electronics", 350, 423)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Firepit", "Outdoors", 75, 98)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Gas Grill", "Outdoors", 150, 5)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Mr Nobody", "Movies", 25, 199)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Inception", "Movies", 30, 76)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pickles", "Produce", 4, 999)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Squash", "Produce", 2, 876)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Intermediate Javascript", "Books", 50, 34)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("SQL for Dummies", "Books", 43, 534)
